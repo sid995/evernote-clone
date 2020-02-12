a4e21bf1d8337dfd5449b3a9cd742265ae4f50ed
Siddharth Kundu <sk31395@gmail.com>
2020-02-12 23:57:46 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -4,22 +4,13 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const firebase = require('firebase')
-require('firebase/firestore')
-
-const firebaseConfig = {
-	apiKey: 'AIzaSyC2Piv5GlHvmXLucodjcfpBeR5POdcZDMo',
-	authDomain: 'evernote-67986.firebaseapp.com',
-	databaseURL: 'https://evernote-67986.firebaseio.com',
-	projectId: 'evernote-67986',
-	storageBucket: 'evernote-67986.appspot.com',
-	messagingSenderId: '519377749683',
-	appId: '1:519377749683:web:b5390e3892eaa346a552c6',
-	measurementId: 'G-2MW9ZCQT63'
+{
+	/*  Add firebase Script between these lines */
 }
 
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
+{
+	/*  Add firebase Script between these lines */
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
